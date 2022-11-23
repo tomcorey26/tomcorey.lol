90de6cfdd8f2417323c3b37cd7d9488710a8f1d2
tomcorey26 <tomcorey26@my.uri.edu>
2022-11-22 23:49:49 -0500

    feat(home): Customize particles

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -28,9 +28,49 @@ export const ParticleBackground = () => {
       loaded={particlesLoaded}
       options={{
         fullScreen: { enable: true, zIndex: -1 },
+
         background: {
           color: {
             value: '#14213D',
+          },
+        },
+
+        particles: {
+          color: {
+            value: '#fff',
+          },
+          links: {
+            color: '#fca311ff',
+            distance: 180,
+            enable: true,
+            opacity: 0.5,
+            width: 2,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outModes: {
+              default: 'bounce',
+            },
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 90,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            value: { min: 1, max: 3 },
           },
         },
         fpsLimit: 120,
@@ -48,53 +88,12 @@ export const ParticleBackground = () => {
           },
           modes: {
             push: {
-              quantity: 4,
+              quantity: 2,
             },
             repulse: {
               distance: 200,
-              duration: 0.4,
+              duration: 0.8,
             },
-          },
-        },
-        particles: {
-          color: {
-            value: '#ffffff',
-          },
-          links: {
-            color: '#ffffff',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outModes: {
-              default: 'bounce',
-            },
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: 'circle',
-          },
-          size: {
-            value: { min: 1, max: 5 },
           },
         },
         detectRetina: true,

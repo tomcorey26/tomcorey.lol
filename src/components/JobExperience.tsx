3d153da366d05c3499b61ff13ea5607945a1be06
tomcorey26 <tomcorey26@my.uri.edu>
2022-11-27 10:23:11 -0500

    feat(job-experience): add styling to point list items

---
+++ src/components/JobExperience.tsx
@@ -0,0 +1,56 @@
+import { Card } from './Card';
+
+import '../styles/job-experience.css';
+
+type Props = JobExperience;
+
+const getMonthName = (month: number) => {
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  return monthNames[month];
+};
+
+const formatDate = (date: Date) =>
+  `${getMonthName(date.getMonth())} ${date.getFullYear()}`;
+
+export default function JobExperience(job: Props) {
+  return (
+    <Card>
+      <div className="job">
+        <img src="" alt="" />
+        <h2 className="job__title">{job.title}</h2>
+        <h3 className="job__company">
+          {job.emoji} {job.company}
+        </h3>
+        <i>
+          {formatDate(job.startDate)} - {formatDate(job.endDate)}
+        </i>
+        <ul className="job__points">
+          {job.points.map((point) => (
+            <li>
+              <p>{point}</p>
+            </li>
+          ))}
+        </ul>
+        <ul className="job__skills">
+          {/* TODO add icons for each skill */}
+          {job.skills.map((skill) => (
+            <li>{skill}</li>
+          ))}
+        </ul>
+      </div>
+    </Card>
+  );
+}
---
+++ src/pages/types.d.ts
@@ -0,0 +1,9 @@
+interface JobExperience {
+  title: string;
+  company: string;
+  emoji: string;
+  startDate: Date;
+  endDate: Date;
+  points: string[];
+  skills: string[];
+}

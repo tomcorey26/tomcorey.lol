82ee742a081e50d173f8132b8b559e5662f87915
tomcorey26 <tomcorey26@my.uri.edu>
2022-12-23 13:22:00 -0500

    remove netlify ssr config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [react(), mdx(), sitemap()],
-  output: "server",
-  adapter: netlify()
 });

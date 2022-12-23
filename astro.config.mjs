dc10860e89a8feb38b98ce66bd4bfd054753528f
tomcorey26 <tomcorey26@my.uri.edu>
2022-12-23 12:37:32 -0500

    Add netlify ssr config bc for some reason static build isnt working :(

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [react(), mdx(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
